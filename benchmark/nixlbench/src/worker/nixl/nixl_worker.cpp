42741f161bb245d08ff4868c0531fc4ae3a0e160
omer-b1 <Omer.B1@dell.com>
2026-01-13 22:06:11 +0200

    benchmark/nixlbench: Fixes #1152 - GUSLI auto-generated conf (#1193)
    Fix incorrect auto-generated GUSLI config file
    
    Signed-off-by: Omer Brezel <omer.b1@dell.com>
    Co-authored-by: Adit Ranadive <aranadive@nvidia.com>

--- benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
+++ benchmark/nixlbench/src/worker/nixl/nixl_worker.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -79,10 +79,10 @@ generateGusliConfigFile(const std::vector<GusliDeviceConfig> &devices) {
     config << "# Config file\nversion=1\n";
 
     for (const auto &dev : devices) {
-        // Format: "id type access_mode shared_exclusive path security_flags"
-        // Example: "11 F W N ./store0.bin sec=0x3"
+        // Format: "id type access_mode direct_io path security_flags"
+        // Example: "11 F W D ./store0.bin sec=0x3"
         config << dev.device_id << " " << dev.device_type << " "
-               << "W N " // Write mode, Not shared (exclusive)
+               << "W D " // Write mode, Direct I/O
                << dev.device_path << " " << dev.security_flags << "\n";
     }
 
