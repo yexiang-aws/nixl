200f6499c573ed0d54d2c308f552efb73ad45556
ovidiusm <ovidium@nvidia.com>
2026-03-19 17:45:04 +0100

    Enable UCX EP support in python wheel build (#1440)
    * Enable NIXL EP build
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Enable UCX experimental API and pin Meson to /usr/local UCX for nixl_ep build
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Update UCX_REF in script as well
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Set UCX version to v1.21.x
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Fix build scripts
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Revert unrelated change
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Pass BUILD_NIXL_EP through the wheel build scripts
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Fix copyright
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Fix script
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Package EP code
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Add torch dep
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Exclude torch cuda lib
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Exclude doca libs
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Bump DOCA to 3.2.1 due to bad link
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    * Remove DOCA dep from EP
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>
    
    ---------
    
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>

--- contrib/build-container.sh
+++ contrib/build-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,8 +35,8 @@ ARCH=$(uname -m)
 WHL_BASE=manylinux_2_39
 WHL_PLATFORM=${WHL_BASE}_${ARCH}
 WHL_PYTHON_VERSIONS="3.12"
-UCX_REF=${UCX_REF:-v1.20.x}
-BUILD_NIXL_EP="false"
+UCX_REF=${UCX_REF:-v1.21.x}
+BUILD_NIXL_EP="true"
 OS="ubuntu24"
 NPROC=${NPROC:-$(nproc)}
 if [ "$CI" = "true" ]; then
